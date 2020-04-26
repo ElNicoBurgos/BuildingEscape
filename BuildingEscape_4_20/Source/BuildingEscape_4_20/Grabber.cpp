b665c4185ad9d0744aceeb937397f9e963d848d0
Nicolasb813 <nicolasb813@gmail.com>
2020-04-26 19:30:47 -0300

    BE29 Using DrawDebugLine

--- BuildingEscape_4_20/Source/BuildingEscape_4_20/Grabber.cpp
+++ BuildingEscape_4_20/Source/BuildingEscape_4_20/Grabber.cpp
@@ -3,6 +3,7 @@
 #include "Grabber.h"
 #include "Engine/World.h"
 #include "GameFramework/PlayerController.h"
+#include "DrawDebugHelpers.h"
 #define OUT
 
 
@@ -29,6 +30,19 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 	
 	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(OUT PlayerViewPointLocation, OUT PlayerViewPointRotation);
 	
-	UE_LOG(LogTemp, Warning, TEXT("Location: %s - Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString() );
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s - Rotation: %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
+
+	//FVector LineTraceEnd = PlayerViewPointLocation + FVector(1.f * Reach, 0.f, 0.f);
+	FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotation.Vector() * Reach);
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);
 }
 
