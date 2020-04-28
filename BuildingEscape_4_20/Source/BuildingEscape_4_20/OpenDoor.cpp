aa7e1d35a69a20562226ad5f657717482d5cd937
Nicolasb813 <nicolasb813@gmail.com>
2020-04-28 16:36:37 -0300

    BE41 Introducing Unreal’s TArray

--- BuildingEscape_4_20/Source/BuildingEscape_4_20/OpenDoor.cpp
+++ BuildingEscape_4_20/Source/BuildingEscape_4_20/OpenDoor.cpp
@@ -4,6 +4,7 @@
 #include "GameFramework/Actor.h"
 #include "Engine/World.h"
 #include "GameFramework/PlayerController.h"
+#define OUT
 
 
 // Sets default values for this component's properties
@@ -22,7 +23,7 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 	//
-	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+	
 	Owner = GetOwner();
 }
 
@@ -40,7 +41,7 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	if (PressurePlate && PressurePlate->IsOverlappingActor(ActorThatOpens))
+	if (PressurePlate && GetTotalMassOfActorsOnPlate() > 60.f)
 	{
 		UE_LOG(LogTemp, Warning, TEXT ("HEEEE"));
 		OpenDoor();
@@ -53,3 +54,11 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	}
 }
 
+float UOpenDoor::GetTotalMassOfActorsOnPlate()
+{
+	float TotalMass = 0.f;
+	TArray<AActor *> OverlapingActors;
+	PressurePlate->GetOverlappingActors(OUT OverlapingActors);
+
+	return TotalMass;
+}
--- BuildingEscape_4_20/Source/BuildingEscape_4_20/OpenDoor.h
+++ BuildingEscape_4_20/Source/BuildingEscape_4_20/OpenDoor.h
@@ -41,6 +41,6 @@ private:
 	float LastDoorOpenTime;
 
 	AActor *Owner;
-	AActor *ActorThatOpens;
-
+	
+	float GetTotalMassOfActorsOnPlate();
 };
